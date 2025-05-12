d6a97c0165ddfc433db718b8f8073ff2d8375285
siam114 <smsiamislam598@gmail.com>
2025-05-13 01:56:12 +0600

    complete routing work

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import Error from './../pages/Error';
 import Navbar from '../layouts/Navbar';
+import BooksView from '../features/books/BooksView';
+import AddBook from '../features/books/AddBook';
 
 const Index = () => {
   return <BrowserRouter>
   <Navbar/>
      <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/show-books" element={<BooksView/>}/>
+        <Route path="/add-book" element={<AddBook/>}/>
         <Route path="*" element={<Error/>}/>
      </Routes>
   </BrowserRouter>
