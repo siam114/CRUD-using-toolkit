6a9deee6888d9feb21bcd03ee9a19bb90afd42e3
siam114 <smsiamislam598@gmail.com>
2025-05-13 13:45:16 +0600

    set form set author

--- src/features/books/AddBook.jsx
+++ src/features/books/AddBook.jsx
@@ -2,13 +2,18 @@ import React, { useState } from 'react'
 
 const AddBook = () => {
   const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
   return (
     <div>
       <h2>Add Book</h2>
       <form>
         <div className='form-field'>
-          <label htmlFor="title">Title</label>
-          <input type="text" id='title' name='title' value={title}/>
+          <label htmlFor="title">Title: </label>
+          <input type="text" id='title' name='title' value={title} onChange={(e)=> setTitle(e.target.value)} required/>
+        </div>
+        <div className='form-field'>
+          <label htmlFor="author">Author: </label>
+          <input type="text" id='author' name='author' value={author} onChange={(e)=> setAuthor(e.target.value)} required/>
         </div>
       </form>
     </div>
