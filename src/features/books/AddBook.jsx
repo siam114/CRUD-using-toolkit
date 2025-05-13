d1c787e60e0a6d9c51d436139b614766972534cb
siam114 <smsiamislam598@gmail.com>
2025-05-13 13:51:36 +0600

    get number of books

--- src/features/books/AddBook.jsx
+++ src/features/books/AddBook.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 
 const AddBook = () => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
+
+  const numberOfBooks = useSelector((state) => state.booksReducer.books.length)
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const book = {id: numberOfBooks + 1, title, author}
+  }
+
   return (
     <div>
       <h2>Add Book</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className='form-field'>
           <label htmlFor="title">Title: </label>
           <input type="text" id='title' name='title' value={title} onChange={(e)=> setTitle(e.target.value)} required/>
@@ -15,6 +24,7 @@ const AddBook = () => {
           <label htmlFor="author">Author: </label>
           <input type="text" id='author' name='author' value={author} onChange={(e)=> setAuthor(e.target.value)} required/>
         </div>
+        <button type='submit'>Add Book</button>
       </form>
     </div>
   )
