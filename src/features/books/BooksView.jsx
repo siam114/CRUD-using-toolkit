b4f8d63ace6e46cbc168370d05999f2755eb9efe
siam114 <smsiamislam598@gmail.com>
2025-05-13 13:34:38 +0600

    design table show book

--- src/features/books/BooksView.jsx
+++ src/features/books/BooksView.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const BooksView = () => {
+  const books = useSelector((state)=>state.booksReducer.books);
+
   return (
     <div>
       <h2>List of Books</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   )
 }
