7ac5b9d0f2ca3be0273c61788752fea78f82c0aa
siam114 <smsiamislam598@gmail.com>
2025-05-13 13:40:39 +0600

    design add book

--- src/features/books/AddBook.jsx
+++ src/features/books/AddBook.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddBook = () => {
+  const [title, setTitle] = useState('')
   return (
     <div>
       <h2>Add Book</h2>
+      <form>
+        <div className='form-field'>
+          <label htmlFor="title">Title</label>
+          <input type="text" id='title' name='title' value={title}/>
+        </div>
+      </form>
     </div>
   )
 }
